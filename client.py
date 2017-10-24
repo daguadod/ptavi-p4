6ce3b90ab484e38705059e5816605e285ed4e590
David Aguado Dianez <daguadod@alpha30.aulas.gsyc.urjc.es>
2017-10-24 20:41:25 +0200

    añadida pregunta 6 a client.py

--- client.py
+++ client.py
@@ -10,16 +10,18 @@ import sys
 # Constantes. Dirección IP del servidor y contenido a enviar
 SERVER = sys.argv[1]
 PORT = int(sys.argv[2])
+LINEA = sys.argv[3]
 LINE = ''
-for I in sys.argv[3:]:
-    LINE += I + ' '
-
+Usuario = sys.argv[4]
+LINE = 'REGISTER sip: ' + Usuario + ' SIP/2.0\r\n\r\n'
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-    my_socket.connect((SERVER, PORT))
-    print("Enviando:", LINE)
-    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
-    data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    try:
+        my_socket.connect((SERVER, PORT))
+        print("Enviando:", LINE)
+        my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
+        data = my_socket.recv(1024)
+        print('Recibido -- ', data.decode('utf-8'))
 
-print("Socket terminado.")
+    except KeyboardInterrupt:
+        print("Finalizado cliente")
