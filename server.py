a697b429e0c5f01a8281a8c142fa9cf5317b1307
David Aguado Dianez <daguadod@alpha21.aulas.gsyc.urjc.es>
2017-10-26 11:57:25 +0200

    HEcha pregunta 7 en server.py

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 
 import socketserver
 import sys
+import time
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
@@ -20,15 +21,20 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         port = self.client_address[1]
         if doc[0] == 'REGISTER':
             user = doc[2]
+            expires = doc[4].split('\r\n')[0]
             address = str(ip) + ":" + str(port)
-            self.dicc[user] = ['address: ' + address]
-            print (self.dicc)
+            self.dicc[user] = ['address: ' + address, 'Expires: ' + expires]
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-
+            if expires == '0':
+                del self.dicc[user]
+            print(self.dicc)
 if __name__ == "__main__":
     # Listens at localhost ('') port 6001 
     # and calls the EchoHandler class to manage the request
-    PORT = int(sys.argv[1])
+    try:
+        PORT = int(sys.argv[1])
+    except IndexError:
+        sys.exit('Usage: server.py puerto')
     serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
 
     print("Lanzando servidor UDP de eco...")
