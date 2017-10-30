5c5aec635d5bd0b0d5b0f7521324c0e4561f40fc
David Aguado Dianez <daguadod@gamma19.aulas.gsyc.urjc.es>
2017-10-30 09:38:41 +0100

    Hecho apartado 9 y pasado PEP8 a server.py

--- server.py
+++ server.py
@@ -16,11 +16,18 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     Echo server class
     """
     dicc = {}
-    lista = []
+
+    def json2registered(self):
+        try:
+            with open('registered.json', 'r') as outfile_json:
+                json.load(self.dicc, outfile_json)
+        except:
+            self.dicc = {}
+
     def register2json(self):
         with open('registered.json', 'w') as outfile_json:
             json.dump(self.dicc, outfile_json, indent=3)
-        
+
     def handle(self):
         line = self.rfile.read()
         doc = line.decode('utf-8').split(" ")
@@ -34,7 +41,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             expired = tiempo + timedelta(seconds=int(expires))
             address = str(ip) + ":" + str(port)
             fecha = expired.strftime(formato)
-            self.dicc[user] = {'address' : address, 'Expires' : fecha}
+            self.dicc[user] = {'address': address, 'Expires': fecha}
             self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
             if expires == '0':
                 del self.dicc[user]
@@ -45,15 +52,16 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             for user in lista:
                 del self.dicc[user]
             print(self.dicc)
+            print()
             self.register2json()
 if __name__ == "__main__":
-    # Listens at localhost ('') port 6001 
+    # Listens at localhost ('') port 6001
     # and calls the EchoHandler class to manage the request
     try:
         PORT = int(sys.argv[1])
     except IndexError:
         sys.exit('Usage: server.py puerto')
-    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler) 
+    serv = socketserver.UDPServer(('', PORT), SIPRegisterHandler)
 
     print("Lanzando servidor UDP de eco...")
     try:
